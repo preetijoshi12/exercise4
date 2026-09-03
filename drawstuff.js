e92a40e75dd573c3a0e2421adaf51e1b3b38eb2b
preetijoshi12 <joshipreetim@gmail.com>
2026-09-03 17:19:03 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -519,7 +519,7 @@ function main() {
     var view = {eye:testEye, at:testAt, up:new Vector(0,1,0)};
     var poly = [{x:-5,y:5,z:10,c:new Color(255,0,0,255)}, {x:5,y:5,z:10,c:new Color(0,255,0,255)}, 
                 {x:5,y:-5,z:10,c:new Color(0,0,0,255)}, {x:-5,y:-5,z:10,c:new Color(0,0,255,255)}];
-    */
+    
 
     // Diamond View
     var testEye = new Vector(0,0,0);
@@ -533,9 +533,9 @@ function main() {
         {x:5,  y:5, z:10, c:new Color(0,255,0,255)},   // Green
         {x:5,  y:-5, z:10, c:new Color(0,0,0,255)},    // Black
         {x:-5, y:-5, z:10, c:new Color(0,0,255,255)}   // Blue
-    ];
+    ];*/
 
-    /** / Angle
+    // Angle
     var testEye = new Vector(-2, -1, 0); // Offset camera position
     var testAt = Vector.subtract(new Vector(0,0,10), testEye);
     var view = { eye: testEye, at: testAt, up: new Vector(0, 1, 0) };
@@ -545,7 +545,7 @@ function main() {
         {x:5,  y:5, z:10, c:new Color(0,255,0,255)},   // Green
         {x:5,  y:-5, z:10, c:new Color(0,0,0,255)},    // Black
         {x:-5, y:-5, z:10, c:new Color(0,0,255,255)}   // Blue
-    ]; */
+    ];
     
     // Define and render a rectangle in 2D with colors and coords at corners
     projectPoly(imagedata,poly,view);
